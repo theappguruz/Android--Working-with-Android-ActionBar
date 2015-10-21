de82f379dffc91ccbb009ea896b7a71731bd8337
Bharat <bharatappguru@gmail.com>
2015-10-21 17:59:14 +0530

    Updated

--- app/src/main/java/com/tagworld/myactionbar/MainActivity.java
+++ app/src/main/java/com/tagworld/myactionbar/MainActivity.java
@@ -31,22 +31,22 @@ public class MainActivity extends Activity implements OnNavigationListener {
 
 		// Hide the action bar title
 		actionBar.setBackgroundDrawable(new ColorDrawable(Color.GRAY));
-		actionBar.setDisplayShowTitleEnabled(false);
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-
-		final String[] dropdownValues = getResources().getStringArray(
-				R.array.dropdown);
-
-		// Specify a SpinnerAdapter to populate the dropdown list.
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-				actionBar.getThemedContext(),
-				android.R.layout.simple_spinner_item, android.R.id.text1,
-				dropdownValues);
-
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-		// Set up the dropdown list navigation in the action bar.
-		actionBar.setListNavigationCallbacks(adapter, this);
+//		actionBar.setDisplayShowTitleEnabled(false);
+//		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+//
+//		final String[] dropdownValues = getResources().getStringArray(
+//				R.array.dropdown);
+//
+//		// Specify a SpinnerAdapter to populate the dropdown list.
+//		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+//				actionBar.getThemedContext(),
+//				android.R.layout.simple_spinner_item, android.R.id.text1,
+//				dropdownValues);
+//
+//		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//
+//		// Set up the dropdown list navigation in the action bar.
+//		actionBar.setListNavigationCallbacks(adapter, this);
 	}
 
 	@Override
